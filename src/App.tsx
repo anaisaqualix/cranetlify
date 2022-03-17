4e071a06bf43fef26ee72a6b10cfeabd5a2aa2f4
Anaisa <anaisa@qualix.inf.br>
2022-03-17 15:52:07 -0300

    add title

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          H&S Tecnologia
         </a>
       </header>
     </div>
